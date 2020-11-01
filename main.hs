b8e8c6c21e8d6788655de0b6e503a6b883c1fec1
yankayohana <yanka.neves@ccc.ufcg.edu.br>
2020-11-01 11:35:26 -0300

    funcoes backend

--- main.hs
+++ main.hs
@@ -2,6 +2,7 @@ module Main where
 import qualified Utils as Utils
 --import System.Console.ANSI
 import System.IO
+import System.Directory
 
 -- MENU  
 
@@ -45,3 +46,101 @@ opcaoEscolhida opcao
     | opcao == 7 = do {{-visualizaListaDesejos-}; menu}  
     | opcao == 8 = do {{-visualizaInfoLivros-}; menu}  
     | otherwise =  do {putStrLn "Opcao invalida, Porfavor escolha uma opcao valida \n" ; menu}
+
+
+-- FUNÇÕES BACKEND
+
+data Livro = Livro {
+nome :: String,
+genero :: String,
+ano :: String,
+sinopse :: String,
+editora :: String,
+autor :: String,
+nPaginas :: Integer
+} deriving (Show)
+
+livros = [Livro { nome = "aa", genero = "aa", ano = "aaaa", sinopse = "aaa", editora = "aaa", autor = "aaaa", nPaginas = 99 }, Livro { nome = "bb", genero = "bb", ano = "bbbb", sinopse = "bbb", editora = "bbb", autor = "bbbb", nPaginas = 987 }]
+
+
+imprimeTodosLivros :: IO ()
+imprimeTodosLivros = do
+  putStrLn(listarLivros livros)
+
+
+listarLivros :: [Livro] -> String
+listarLivros [] = ""
+listarLivros (x:xs) = toStringLivro x ++ ['\n'] ++ listarLivros xs
+
+
+toStringLivro :: Livro -> String
+toStringLivro (Livro {nome = n, genero = g, ano = a, sinopse = s, editora = e, autor = au, nPaginas = np}) = show n ++ " - " ++ au ++ " - " ++ a
+
+
+getLivroByIndex :: Int -> [Livro] -> Livro
+getLivroByIndex 0 (x:xs) = x
+getLivroByIndex n (x:xs) = getLivroByIndex (n-1) xs
+
+
+addLivroLendo :: Int -> IO ()
+addLivroLendo i = appendFile "lendo.txt" (intToFileContent i)
+
+
+intToFileContent :: Int -> String
+intToFileContent numero = ((show numero) ++ "\n")
+
+listarLivrosLendo :: IO [Livro]
+listarLivrosLendo = do
+  ls <- leituras
+  return $ filtraLivros ls livros []
+
+
+leituras :: IO [Int]
+leituras = do
+  conteudo <- readFile "lendo.txt"
+  let linhas = lines conteudo
+  let indexes = fmap (read::String->Int) linhas
+  return indexes
+
+finalizados :: IO [Int]
+finalizados = do
+  conteudo <- readFile "finalizado.txt"
+  let linhas = lines conteudo
+  let indexes = fmap (read::String->Int) linhas
+  return indexes
+
+removerLendo :: Int -> IO ()
+removerLendo i = do
+  ls <- leituras
+  let a = removerLendo' i [] ls 
+  removeFile "lendo.txt"
+  rebuildLendo a
+  appendFile "lendo.txt" ""
+  where
+    removerLendo' :: Int -> [Int] -> [Int] -> [Int]
+    removerLendo' i filtered (x:xs)
+      |  i == x = filtered ++ xs
+      | otherwise = removerLendo' i (filtered ++ [x]) xs
+
+rebuildLendo :: [Int] -> IO ()
+rebuildLendo [] = return ()
+rebuildLendo (x:xs) = do 
+  addLivroLendo x
+  rebuildLendo xs
+
+finalizaLivro :: Int -> IO()
+finalizaLivro i = do
+  removerLendo i
+  addLivroFinalizado i
+
+addLivroFinalizado :: Int -> IO ()
+addLivroFinalizado i = appendFile "finalizado.txt" (intToFileContent i)
+
+listarLivrosFinalizados :: IO [Livro]
+listarLivrosFinalizados = do
+  ls <- finalizados
+  return $ filtraLivros ls livros []
+
+filtraLivros :: [Int] -> [Livro] -> [Livro] -> [Livro]
+filtraLivros [] livros livrosFiltrados = livrosFiltrados
+filtraLivros (x:xs) livros livrosFiltrados = filtraLivros xs livros (livrosFiltrados ++ [getLivroByIndex x livros])
