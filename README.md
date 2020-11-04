acd72410b65879198669236ed3fc0a6d13c41f17
Angela Sousa <angela.sousa@ccc.ufcg.edu.br>
2020-11-03 23:51:05 -0300

    itens menu

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # BOOKFLIX
 Repositório do projeto da disciplina Paradigmas de Linguagens de Programação
+
+--------------------- Acompanhamento de Leitura ------------------------
+  
+========================================================================
+Disciplina: Paradigmas de Linguagem de Programação
+Alunas: Yanka Yohana e Angela Sousa
+               
+========================================================================
+
+Descrição:
+
+Sistema de acompanhamento pessoal de leitura, onde o usuário pode cadastrar novos livros que estiver lendo e acompanhar seu progresso em cada um. 
+
+
+Linguagens:
+
+Haskell
+Prolog
+
+Detalhes:
+Instalar: cabal install ansi-terminal 
--- main.hs
+++ main.hs
@@ -1,7 +1,7 @@
 module Main where
 import qualified Utils as Utils
 import System.IO.Unsafe(unsafeDupablePerformIO)
---import System.Console.ANSI
+import System.Console.ANSI
 import System.IO
 import System.Directory
 
@@ -9,14 +9,14 @@ import System.Directory
 
 main :: IO()
 main = do
-  putStrLn (Utils.logo)
- -- threadDelay 5000 comando para esperar alguns segundos depois q printar o logo na tela, para depois printar o menu, tbm faz parte do import q n ta pegando
- -- clearScreen  comando de limpeza de tela, não sei pk n tá funcionando aqui, todo o import do System.Console.ANSI n tá pegando na verdade, Vê se tu consegue aí
-  menu
+    putStrLn (Utils.logo)
+    --threadDelay 5000 
+    clearScreen  
+    menu
   
 menu :: IO ()
 menu = do
-   -- clearScreen
+    clearScreen
     putStrLn "1 - Cadastrar novo ebook: "
     putStrLn "2 - Visualizar lista de ebooks lidos: "
     putStrLn "3 - Visualizar lista de ebooks finalizados: "
@@ -24,7 +24,8 @@ menu = do
     putStrLn "5 - Dar nota à um ebook: "
     putStrLn "6 - Adicionar livro à lista de desejos de leitura: "
     putStrLn "7 - Visualizar lista de desejos: "
-    putStrLn "8 - Visuaizar informações sobre ebook: "
+    putStrLn "8 - Visuaizar informações sobre ebooks do sistema: " -- aqui pega só os estáticos
+    putStrLn "8 - Visuaizar informações sobre ebooks cadastrados: " -- aqui pega os cadastrados, tem q ser uma lista variável
     putStrLn "Ou Digite 0 Para Sair: " 
     putStrLn "\nOpcao: "
     opcao <- getLine
@@ -34,14 +35,15 @@ menu = do
 
 opcaoEscolhida :: Int -> IO()
 opcaoEscolhida opcao 
-    | opcao == 1 = do {{-cadastrarLivro-}; menu} -- esse menu nas opções de 1 a 8 tá retornando para o menu, é só p não dar erro, só tirar a medida q formos implementando as funções
+    | opcao == 1 = do {cadastrarLivro}
     | opcao == 2 = do {{-visualizaLivrosLidos-} ; menu}
-    | opcao == 3 = do {{-visualizaLivrosFinalizados-} ; menu}
+    | opcao == 3 = do {{-listarLivrosFinalizados-}; menu}
     | opcao == 4 = do {{-marcaFinalizado-}; menu}
     | opcao == 5 = do {{-notaLivro-}; menu}
     | opcao == 6 = do {enviarLivroDesejado}    
     | opcao == 7 = do {listarListaDesejos}  
-    | opcao == 8 = do {{-visualizaInfoLivros-}; menu}  
+    | opcao == 8 = do {listarLivrosCadastrados} -- aqui tem um conflito: para cadastrar tem q ser uma lista variável, 
+    | opcao == 9 = do {imprimeTodosLivros} -- fiz o cadastrar e deixei o estático, mas temos q ver como unir, se precisar
     | opcao == 0 = do {putStrLn"Saindo..."}
     | otherwise =  do {putStrLn "Opcao invalida, Porfavor escolha uma opcao valida \n" ; menu}
 
@@ -58,8 +60,13 @@ autor :: String,
 nPaginas :: Integer
 } deriving (Show)
 
-livros = [Livro { nome = "aa", genero = "aa", ano = "aaaa", sinopse = "aaa", editora = "aaa", autor = "aaaa", nPaginas = 99 }, Livro { nome = "bb", genero = "bb", ano = "bbbb", sinopse = "bbb", editora = "bbb", autor = "bbbb", nPaginas = 987 }]
+-- Já disponíveis no sistema
 
+livros = [Livro { nome = "Harry potter e o calice de fogo", genero = "Fantasia", ano = "2001", sinopse = "A chegada na Escola e a notícia de que esta sediará o importante Torneio Tribruxo.", editora = "Rocco", autor = "J.K. Rowling", nPaginas = 584 }, 
+          Livro { nome = "Harry potter e o enigma do principe", genero = "Fantasia", ano = "2005", sinopse = "Um misterioso príncipe mestiço ajuda Harry em sua aula de Poções.", editora = "Rocco", autor = "J.K. Rowling", nPaginas = 512 },
+          Livro { nome = "A revolucao dos bichos: Um conto de fadas", genero = "Infantil", ano = "2007", sinopse = "Intrigante narrativa protagonizada geralmente por animais, mas que reflete ações humanas com algum ensinamento de cunho moral." , editora = "Companhia das Letras", autor = "George Orwell", nPaginas = 152 },
+          Livro { nome = "O iluminado", genero = "Terror/Romance", ano = "2012", sinopse = "A luta assustadora entre dois mundos. Um menino e o desejo assassino de poderosas forças malignas." , editora = "Suma", autor = " Stephen King", nPaginas = 464 },
+          Livro { nome = "Sherlock Holmes - Um estudo em vermelho", genero = "Acao e Aventura", ano = "2019", sinopse = "Holmes é chamado para solucionar o caso de um homem que foi encontrado morto, com uma expressão de terror, mas que não apresenta ferimentos, apenas manchas de sangue pelo corpo." , editora = "Principis", autor = "Arthur Conan Doyle", nPaginas = 176 }]
 
 imprimeTodosLivros :: IO ()
 imprimeTodosLivros = do
@@ -163,3 +170,28 @@ listarListaDesejos = do
     Utils.printEspaco
 
 
+cadastrarLivro :: IO()
+cadastrarLivro = do
+    Utils.printEspaco
+    putStr "== Digite o nome do livro que deseja cadastrar: "
+    nome <- getLine
+    putStr "== Digite o autor do livro que deseja cadastrar: "
+    autor <- getLine
+    putStr "== Digite o ano do livro que deseja cadastrar: "
+    ano <- getLine
+    putStr "== Digite a editora do livro que deseja cadastrar: "
+    editora <- getLine
+    putStr "== Digite a sinopse do livro que deseja cadastrar: "
+    sinopse <- getLine
+    putStr "== Digite o numero de paginas do livro que deseja cadastrar: "
+    nPaginas <- getLine
+    appendFile "listaLivros.txt" (nome ++ " - " ++ autor  ++ " - " ++ ano  ++ " - " ++ editora  ++ " - " ++ sinopse  ++ " - " ++ nPaginas  ++ " \n")
+    Utils.printEspaco
+
+listarLivrosCadastrados :: IO()
+listarLivrosCadastrados = do
+    Utils.printEspaco
+    putStrLn "==== Sua lista de livros: ====\n"  
+    listaLivros <- readFile "listaLivros.txt"
+    putStrLn listaLivros
+    Utils.printEspaco
